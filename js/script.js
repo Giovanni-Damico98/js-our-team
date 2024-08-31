016300536e403f7ccc456f1c28120c3feb6dae6c
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-31 20:02:39 +0200

    html + js progress

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 console.log("JS OK");
+const teamMembers = [
+  {
+    id: 1,
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    id: 2,
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    id: 3,
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+  {
+    id: 4,
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    id: 5,
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+  },
+  {
+    id: 6,
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg ",
+  },
+];
